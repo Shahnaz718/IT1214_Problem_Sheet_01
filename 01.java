1a02a64b881640e589c45b74cb533e4ac16d2484
Shahnaz718 <shahnazaatham123@gmail.com>
2025-05-30 05:54:13 +0000

    update

--- 01.java
+++ 01.java
@@ -67,16 +67,16 @@ class Main {
     public static void main(String[] args) {
         Classroom classroom = new Classroom();
         
-        // Add students
+        
         classroom.addStudent(new Student(101, "Alice Smith", 12));
         classroom.addStudent(new Student(102, "Bob Jones", 15));
         classroom.addStudent(new Student(103, "Carol Lee", 10));
         
-        // Update attendance
+        
         classroom.updateAttendance(102, 16);
         classroom.updateAttendance(104, 5); // Should show not found
         
-        // Display all students
+        
         classroom.displayAllStudents();
     }
 }
